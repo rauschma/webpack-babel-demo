0252b465f2d156abdef9aadc7518ee25eb60285b
rauschma <axel@rauschma.de>
2015-11-30 23:36:57 +0100

    First version

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const WORLD = 'world';
+
+document.getElementById('output').innerHTML = `Hello ${WORLD}!`;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/* global __dirname */
+
+var path = require('path');
+
+var webpack = require('webpack');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var dir_js = path.resolve(__dirname, 'js');
+var dir_html = path.resolve(__dirname, 'html');
+var dir_build = path.resolve(__dirname, 'build');
+
+module.exports = {
+    entry: path.resolve(dir_js, 'main.js'),
+    output: {
+        path: dir_build,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: dir_build,
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: dir_js,
+                query: {
+                    presets: ['es2015'],
+                },
+            }
+        ]
+    },
+    plugins: [
+        // Simply copies the files over
+        new CopyWebpackPlugin([
+            { from: dir_html } // to: output.path
+        ]),
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map',
+};
